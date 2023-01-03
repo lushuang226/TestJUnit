031377a95e6de5106b82321994ba90097ddff04d
盧爽 <t108aea006@ntut.edu.tw>
2023-01-03 19:03:26 +0800

    add main

--- src/main/java/test/Score.java
+++ src/main/java/test/Score.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @description:
  * @projectName:TestJUnit
@@ -9,6 +13,20 @@ package test;
  * @version:1.0
  */
 public class Score {
+    public static void main(String[] args) {
+        System.out.print("Enetr the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            int score = Integer.parseInt(br.readLine());
+            char grade = letterGrade(score);
+            System.out.println("The grade of " + score + " is " + grade); } catch (NumberFormatException ex) {
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static char letterGrade(int score) {
         char grade;
         if (score <0 || score > 100)
